f31a13fa9110804b75ae569a68e667b83ccaabc8
sebski123 <sebthomle@gmail.com>
2022-02-12 23:15:33 +0100

    add path for static site

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token("body", function getId(req) {
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
