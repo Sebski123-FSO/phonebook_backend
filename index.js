39c23d109e0d726893a830bf3f75bd7dab69f30b
sebski123 <sebthomle@gmail.com>
2022-02-14 00:51:57 +0100

    added linter

--- index.js
+++ index.js
@@ -28,29 +28,6 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
@@ -81,7 +58,7 @@ app.post("/api/persons", (req, res, next) => {
 
       newPerson
         .save()
-        .then((result) => {
+        .then(() => {
           res.status(200).json(newPerson);
         })
         .catch((error) => next(error));
@@ -113,7 +90,7 @@ app.put("/api/persons/:id", (req, res, next) => {
 
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then((response) => {
+    .then(() => {
       res.status(204).end();
     })
     .catch((error) => next(error));
